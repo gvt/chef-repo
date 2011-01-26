6a80d6ffb7428bc79a08613fa9c168b1664a936c
Gregory Tomei <gregory@websystemstudio.com>
2011-01-26 11:58:43 -0800

    tweaking script, had usage of test backwards

--- netpop-reporting-setup-env/recipes/default.rb
+++ netpop-reporting-setup-env/recipes/default.rb
@@ -18,13 +18,13 @@ deploy_to = "/srv/netpop-reporting"
 bash "create group for apps" do
   new_group = "apps"
   code "addgroup #{new_group}"
-  not_if "test -z `grep #{new_group} /etc/group`" # only if not already exists
+  not_if "test -n `grep #{new_group} /etc/group`" # not if group already exists
 end
 
 bash "create user account for reporting" do
   new_user = "reporting"
   code "adduser --ingroup apps #{new_user}"
-  not_if "test -z `grep #{new_user} /etc/passwd`" # only if not already exists
+  not_if "test -n `grep #{new_user} /etc/passwd`" # not if user account already exists
 end
 
 directory deploy_to do
