c17a3b75d9c30268aa78b272080f998ef5460490
Gregory Tomei <gregory@websystemstudio.com>
2011-01-25 16:41:14 -0800

    first attempt at chef recipe to configure the EC2 instance

---
+++ netpop-reporting-setup-env/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Gregory Tomei"
+maintainer_email "gregory@websystemstudio.com"
+license          "All rights reserved"
+description      "Installs/Configures netpop-reporting-setup-env"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
---
+++ netpop-reporting-setup-env/recipes/default.rb
@@ -0,0 +1,32 @@
+##
+#
+# Cookbook Name:: netpop-reporting-setup-env
+# Recipe:: default
+#
+# Copyright 2011, Web System Studio, Inc
+#
+# All rights reserved - Do Not Redistribute
+#
+##
+# what it does:
+#   1) set directory permissions to allow for capistrano deployment
+#   2) install server software needed by the app (monit, DJ, etc)
+##
+
+deploy_to = "/srv/netpop-reporting"
+
+bash "create group for apps" do
+  code "addgroup apps"
+end
+
+bash "create user account for reporting" do
+  code "adduser --in-group apps reporting"
+end
+
+directory deploy_to do
+  user      "reporting"
+  group     "apps"
+  mode      0775
+end
+
+include_recipe "monit"
